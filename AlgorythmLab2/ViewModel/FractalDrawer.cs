d77471b499dcb01c1a90c156cc1966ebfa949073
FazylovV <vladislav.fazylov04@gmail.com>
2023-10-18 22:37:35 +0500

    changed FractalDrawer.cs

--- AlgorythmLab2/ViewModel/FractalDrawer.cs
+++ AlgorythmLab2/ViewModel/FractalDrawer.cs
@@ -1,14 +1,5 @@
 ﻿using AlgorythmLab2.Model;
 using AlgorythmLab2.View;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace AlgorythmLab2.ViewModel
 {
