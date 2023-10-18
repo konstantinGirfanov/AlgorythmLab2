81c25a07d2b905036a298c669668fb940a4efbb5
FazylovV <vladislav.fazylov04@gmail.com>
2023-10-18 23:52:46 +0500

    realisation hanoi with stack

--- AlgorythmLab2/Model/HanoiTowers.cs
+++ AlgorythmLab2/Model/HanoiTowers.cs
@@ -10,6 +10,7 @@ namespace AlgorythmLab2.Model
     {
         private List<Tuple<int, int>> transitions;
         private long time;
+        private List<Stage> stages;
 
         public HanoiTowers()
         {
@@ -21,12 +22,25 @@ namespace AlgorythmLab2.Model
             return new List<Tuple<int, int>>(transitions);
         }
 
+        public List<Stage> GetStages()
+        {
+            return stages;
+        }
+
         public void Execute(int countOfRings)
         {
             transitions.Clear();
+            Stage stage = new Stage();
+            for (int i = countOfRings; i > 0 ; i--)
+            {
+                stage.kernel1.Push(i);
+            }
+            
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            SolutionHanoibns(countOfRings, 1, 2, 3);
+            
+            SolutionHanoibns2(countOfRings, stage.kernel1, stage.kernel2, stage.kernel3);
+            
             stopwatch.Stop();
             time = stopwatch.ElapsedMilliseconds;
         }
@@ -34,9 +48,23 @@ namespace AlgorythmLab2.Model
         private void SolutionHanoibns(int k, int a, int b, int c)
         {
             if (k > 1) SolutionHanoibns(k - 1, a, c, b);
+            Stage stage = new Stage();
             transitions.Add(new Tuple<int, int>(a, b));
             if (k > 1) SolutionHanoibns(k - 1, c, b, a);
         }
+        
+        private void SolutionHanoibns2(int k, Stack<int> kernel1, Stack<int> kernel2, Stack<int> kernel3)
+        {
+            if (k > 1) SolutionHanoibns2(k - 1, kernel1, kernel3, kernel2);
+            kernel2.Push(kernel1.Pop());
+            Stage stage = new Stage();
+            stage.kernel1 = kernel1;
+            stage.kernel2 = kernel2;
+            stage.kernel3 = kernel3;
+            stages.Add(stage);
+            if (k > 1) SolutionHanoibns2(k - 1, kernel3, kernel2, kernel1);
+        }
+
 
         public static double[] TakeMeasurements(int countOfRings)
         {
